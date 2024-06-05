47db176886033047fc0e91dc45bc3ced72f82782
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-05 19:01:03 +0200

    Automatycznie wygenerowane klasy

---
+++ AppMobilenBlog/AppMobilenBlog/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AppMobilenBlog.ViewModels;
+using AppMobilenBlog.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppMobilenBlog
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
