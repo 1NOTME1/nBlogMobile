97fbc54f41ac44471df81c2bd27f86d5cfd7a978
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-06 20:59:04 +0200

    Rejstracja Item i User

---
+++ AppMobilenBlog/AppMobilenBlog/Services/ItemDataStore.cs
@@ -0,0 +1,51 @@
+﻿using AppMobilenBlog.ServiceReference;
+using AppMobilenBlog.Services.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMobilenBlog.Services
+{
+    public class ItemDataStore : ADataStore, IDataStore<Post>
+    {
+        readonly List<Post> items;
+
+        public ItemDataStore()
+            : base() => items = nBlogService.PostAllAsync().GetAwaiter().GetResult().ToList();
+
+        public async Task<bool> AddItemAsync(Post post)
+        {
+            items.Add(post);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(Post post)
+        {
+            var oldPost = items.Where((Post arg) => arg.PostId == post.PostId).FirstOrDefault();
+            items.Remove(oldPost);
+            items.Add(post);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(int id)
+        {
+            var oldPost = items.Where((Post arg) => arg.PostId == id).FirstOrDefault();
+            items.Remove(oldPost);
+
+            return await Task.FromResult(true);
+        }
+
+        public async Task<Post> GetItemAsync(int id)
+        {
+            return await Task.FromResult(items.FirstOrDefault(s => s.PostId == id));
+        }
+
+        public async Task<IEnumerable<Post>> GetItemsAsync(bool forceRefresh = false)
+        {
+            return await Task.FromResult(items);
+        }
+    }
+}
---
+++ AppMobilenBlog/AppMobilenBlog/Services/UserDataStore.cs
@@ -0,0 +1,53 @@
+﻿using AppMobilenBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMobilenBlog.Services
+{
+    public class UserDataStore : IDataStore<User>
+    {
+
+        readonly List<User> users;
+
+        public UserDataStore()
+        {
+            users = new List<User>()
+            {
+                new User { UserId = 1, Username = "User1", Email = "user1@example.com", PasswordHash = "hash1", RegistrationDate = DateTime.Now, RoleId = 1 },
+                new User { UserId = 2, Username = "User2", Email = "user2@example.com", PasswordHash = "hash2", RegistrationDate = DateTime.Now.AddDays(-1), RoleId = 2 },
+                new User { UserId = 3, Username = "User3", Email = "user3@example.com", PasswordHash = "hash3", RegistrationDate = DateTime.Now.AddDays(-2), RoleId = 3 }
+            };
+
+        }
+
+        public async Task<bool> AddItemAsync(User item)
+        {
+            users.Add(item);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> UpdateItemAsync(User item)
+        {
+            var oldItem = users.Where((User arg) => arg.UserId == item.UserId).FirstOrDefault();
+            users.Remove(oldItem);
+            users.Add(item);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<bool> DeleteItemAsync(int id)
+        {
+            var oldItem = users.Where((User arg) => arg.UserId == id).FirstOrDefault();
+            users.Remove(oldItem);
+            return await Task.FromResult(true);
+        }
+
+        public async Task<User> GetItemAsync(int id)
+            => await Task.FromResult(users.FirstOrDefault(s => s.UserId == id));
+
+        public async Task<IEnumerable<User>> GetItemsAsync(bool forceRefresh = false)
+            => await Task.FromResult(users);
+    }
+}
