2d3af2cea6b327444c7647b2608ddf1a4101988e
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-07 21:05:38 +0200

    Poprawki dla Widoków usera

---
+++ AppMobilenBlog/AppMobilenBlog/Views/UserView/UserUpdatePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AppMobilenBlog.ViewModels.UserViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMobilenBlog.Views.UserView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserUpdatePage : ContentPage
+    {
+        public UserUpdatePage()
+        {
+            InitializeComponent();
+            BindingContext = new UserUpdateViewModel();
+        }
+    }
+}
