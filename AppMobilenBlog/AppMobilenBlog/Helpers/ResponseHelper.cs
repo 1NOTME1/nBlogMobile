08a80afdf559132eeb0fffd3f6fc7324dde4399b
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-07 21:54:18 +0200

    Nowy helper

---
+++ AppMobilenBlog/AppMobilenBlog/Helpers/ResponseHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AppMobilenBlog.Helpers
+{
+    public static class ResponseHelper
+    {
+        public async static Task<bool> HandleRequest(this Task serviceMethod)
+        {
+            try
+            {
+                await serviceMethod;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+    }
+}
