cd9bcd6dbf3f9fbc677ee3bc4402ac2c98909dc3
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-07 21:06:10 +0200

    Stworzenie Modeli klas

---
+++ AppMobilenBlog/AppMobilenBlog/Models/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppMobilenBlog.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime? PublicationDate { get; set; }
+    }
+}
