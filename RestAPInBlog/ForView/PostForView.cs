f07d1664cbb272e050e1455dd8862edd8dff2692
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-08 15:00:46 +0200

    Stworzenie ForView dla klas Post i User

---
+++ RestAPInBlog/ForView/PostForView.cs
@@ -0,0 +1,37 @@
+﻿using RestAPInBlog.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RestAPInBlog.Model;
+
+namespace RestAPInBlog.ForView
+{
+    public class PostForView
+    {
+        public int PostId { get; set; }
+        public int? UserId { get; set; }
+        public string UserName { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime? PublicationDate { get; set; }
+        public string CategoryData { get; set; }
+        public string TagData { get; set; }
+        public int CommentCount { get; set; }
+        public int LikeCount { get; set; }
+
+        public static implicit operator PostForView(Post source)
+            => new PostForView
+            {
+                UserName = source.User?.Username ?? string.Empty,
+                CategoryData = string.Join(", ", source.Categories.Select(c => c.CategoryName)),
+                TagData = string.Join(", ", source.Tags.Select(t => t.TagName)),
+                CommentCount = source.Comments.Count,
+                LikeCount = source.Likes.Count
+            }.CopyProperties(source);
+
+
+        public static implicit operator Post(PostForView source)
+            => new PostForView().CopyProperties(source);
+    }
+}
---
+++ RestAPInBlog/ForView/UserForView.cs
@@ -0,0 +1,28 @@
+﻿using RestAPInBlog.Helpers;
+using RestAPInBlog.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace RestAPInBlog.ForView
+{
+    public class UserForView
+    {
+        public int UserId { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public int RoleId { get; set; }
+        public string? RoleName { get; set; } // Nazwa roli użytkownika
+        public int PostCount { get; set; } // Liczba postów użytkownika
+        public int CommentCount { get; set; } // Liczba komentarzy użytkownika
+        public int LikeCount { get; set; } // Liczba polubień użytkownika
+
+        public static implicit operator User(UserForView cli)
+             => new User().CopyProperties(cli);
+
+        public static implicit operator UserForView(User source)
+            => new UserForView().CopyProperties(source);
+    }
+}
