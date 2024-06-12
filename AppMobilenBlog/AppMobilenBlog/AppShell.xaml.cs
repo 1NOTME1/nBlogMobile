9f3e91c371c6782d41806349f24f74cb4a3543dc
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-12 20:29:58 +0200

    Rejestracja loginu

--- AppMobilenBlog/AppMobilenBlog/AppShell.xaml.cs
+++ AppMobilenBlog/AppMobilenBlog/AppShell.xaml.cs
@@ -1,4 +1,6 @@
-﻿using AppMobilenBlog.Views;
+﻿using AppMobilenBlog.Services;
+using AppMobilenBlog.Views;
+using AppMobilenBlog.Views.PostView;
 using AppMobilenBlog.Views.UserView;
 using System;
 using Xamarin.Forms;
@@ -10,12 +12,16 @@ namespace AppMobilenBlog
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(PostDetailPage), typeof(PostDetailPage));
             Routing.RegisterRoute(nameof(NewPostPage), typeof(NewPostPage));
+            Routing.RegisterRoute(nameof(PostDetailPage), typeof(PostDetailPage));
             Routing.RegisterRoute(nameof(PostUpdatePage), typeof(PostUpdatePage));
+
+            Routing.RegisterRoute(nameof(NewUserPage), typeof(NewUserPage));
             Routing.RegisterRoute(nameof(UserUpdatePage), typeof(UserUpdatePage));
             Routing.RegisterRoute(nameof(UserDetailPage), typeof(UserDetailPage));
-            Routing.RegisterRoute(nameof(NewUserPage), typeof(NewUserPage));
+
+            DependencyService.Register<ILoginService, LoginService>();
+
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
