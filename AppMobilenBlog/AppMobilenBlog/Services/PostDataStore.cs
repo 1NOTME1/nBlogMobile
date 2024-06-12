bb81ccc34634962534ddb0113bc6eeb0aaa0a318
Michał Kwaśniewski <apparentlynotapplications@gmail.com>
2024-06-12 20:25:58 +0200

    Utwórzenie PostDataStore

---
+++ AppMobilenBlog/AppMobilenBlog/Services/PostDataStore.cs
@@ -0,0 +1,33 @@
+﻿using AppMobilenBlog.Helpers;
+using AppMobilenBlog.ServiceReference;
+using AppMobilenBlog.Services.Abstract;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppMobilenBlog.Services
+{
+    public class PostDataStore : AListDataStore<PostForView>
+    {
+        public PostDataStore()
+           : base()
+           => items = nBlogService.PostAllAsync().GetAwaiter().GetResult().ToList();
+
+        public override PostForView Find(PostForView item)
+            => items.Where((PostForView arg) => arg.PostId == item.PostId).FirstOrDefault();
+
+        public override PostForView Find(int id)
+            => items.FirstOrDefault(s => s.PostId == id);
+
+        public override async Task Refresh()
+            => items = (await nBlogService.PostAllAsync()).ToList();
+
+        public override async Task<bool> DeleteItemFromService(PostForView item)
+            => await nBlogService.PostDELETEAsync(item.PostId).HandleRequest();
+
+        public override async Task<bool> UpdateItemInService(PostForView item)
+            => await nBlogService.PostPUTAsync(item.PostId, item).HandleRequest();
+
+        public override async Task<bool> AddItemToService(PostForView item)
+            => await nBlogService.PostPOSTAsync(item).HandleRequest();
+    }
+}
